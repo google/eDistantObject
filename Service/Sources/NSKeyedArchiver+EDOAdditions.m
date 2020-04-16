81b561055f08986e383f6783fc6cc0c94f8b7642
Sundeep Khandpur <sskhandp@google.com>
2020-04-16 15:17:37 -0700

    Internal change
    PiperOrigin-RevId: 306931173

--- Service/Sources/NSKeyedArchiver+EDOAdditions.m
+++ Service/Sources/NSKeyedArchiver+EDOAdditions.m
@@ -19,20 +19,23 @@
 @implementation NSKeyedArchiver (EDOAdditions)
 
 + (NSData *)edo_archivedDataWithObject:(id)object {
-// In Xcode 10.0, we can use the newer APIs.
+  // In Xcode 10.0, we can use the newer APIs.
 #if (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 101400) || \
     (defined(__TV_OS_VERSION_MAX_ALLOWED) && __TV_OS_VERSION_MAX_ALLOWED >= 120000) ||       \
     (defined(__WATCH_OS_VERSION_MAX_ALLOWED) && __WATCH_OS_VERSION_MAX_ALLOWED >= 120000) || \
     (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 120000)
-  if (@available(iOS 11.0, macOS 10.13, tvOS 11.0, watchOS 4.0, *)) {
+  // Use instancesRespondToSelector check because it's possible that something loads this on a lower
+  // iOS version than what it was built with, in which case the availability macros fail to protect
+  // it.
+  if ([NSKeyedArchiver instancesRespondToSelector:@selector(initRequiringSecureCoding:)]) {
     NSKeyedArchiver *archiver = [[NSKeyedArchiver alloc] initRequiringSecureCoding:NO];
     [archiver encodeObject:object forKey:NSKeyedArchiveRootObjectKey];
     [archiver finishEncoding];
     return archiver.encodedData;
   }
 #endif
-  // This API is deprecated in iOS 12/macOS 10.14, so we suppress warning here in case its
-  // minimum required SDKs are lower.
+  // This API is deprecated in iOS 12/macOS 10.14, so we suppress warning here in case its minimum
+  // required SDKs are lower.
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
   return [NSKeyedArchiver archivedDataWithRootObject:object];
--- Service/Sources/NSKeyedUnarchiver+EDOAdditions.m
+++ Service/Sources/NSKeyedUnarchiver+EDOAdditions.m
@@ -19,12 +19,16 @@
 @implementation NSKeyedUnarchiver (EDOAdditions)
 
 + (id)edo_unarchiveObjectWithData:(NSData *)data {
-// In Xcode 10.0, we can use the newer APIs.
+  // In Xcode 10.0, we can use the newer APIs.
 #if (defined(__MAC_OS_X_VERSION_MAX_ALLOWED) && __MAC_OS_X_VERSION_MAX_ALLOWED >= 101400) || \
     (defined(__TV_OS_VERSION_MAX_ALLOWED) && __TV_OS_VERSION_MAX_ALLOWED >= 120000) ||       \
     (defined(__WATCH_OS_VERSION_MAX_ALLOWED) && __WATCH_OS_VERSION_MAX_ALLOWED >= 120000) || \
     (defined(__IPHONE_OS_VERSION_MAX_ALLOWED) && __IPHONE_OS_VERSION_MAX_ALLOWED >= 120000)
-  if (@available(iOS 11.0, macOS 10.13, tvOS 11.0, watchOS 4.0, *)) {
+  // Use instancesRespondToSelector check because it's possible that something loads this on a lower
+  // iOS version than what it was built with, in which case the availability macros fail to protect
+  // it.
+  SEL selector = @selector(initForReadingFromData:error:);
+  if ([NSKeyedUnarchiver instancesRespondToSelector:selector]) {
     NSKeyedUnarchiver *unarchiver = [[NSKeyedUnarchiver alloc] initForReadingFromData:data
                                                                                 error:nil];
     unarchiver.decodingFailurePolicy = NSDecodingFailurePolicyRaiseException;
