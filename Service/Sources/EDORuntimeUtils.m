a75d424d4c35afa3ef04cee6b8a51fca57e46a30
eDO Team <no-reply@google.com>
2025-02-19 16:15:12 -0800

    Support wrapping SwiftObjects in EDO parameters.
    Previously, one of the ways Swift classes can be "passed" to different processes is to have it conform to a @objc protocol and use the protocol in the shared interface. This approach still required that the Swift class inherit from NSObject. This change loosens this requirement.
    
    PiperOrigin-RevId: 728856266

--- Service/Sources/EDORuntimeUtils.m
+++ Service/Sources/EDORuntimeUtils.m
@@ -6,19 +6,14 @@ NSMethodSignature *EDOGetMethodSignature(id target, SEL sel) {
   // eDO uses the last object of the -forwardingTargetForSelector: chain to perform
   // -methodSignatureForSelector:.
   id forwardedObject = target;
-  Class klass = object_getClass(forwardedObject);
-  Method method = sel ? class_getInstanceMethod(klass, sel) : nil;
-  id nextForwardedObject = [forwardedObject forwardingTargetForSelector:sel];
-  while (method == nil && nextForwardedObject != nil) {
-    forwardedObject = nextForwardedObject;
-    klass = object_getClass(forwardedObject);
-    method = sel ? class_getInstanceMethod(klass, sel) : nil;
-    nextForwardedObject = [forwardedObject forwardingTargetForSelector:sel];
-  }
-
-  if (method) {
-    return [NSMethodSignature signatureWithObjCTypes:method_getTypeEncoding(method)];
-  } else {
-    return [forwardedObject methodSignatureForSelector:sel];
-  }
+  id lastObjectInForwardingChain;
+  do {
+    Class klass = object_getClass(forwardedObject);
+    Method method = sel ? class_getInstanceMethod(klass, sel) : nil;
+    if (method) {
+      return [NSMethodSignature signatureWithObjCTypes:method_getTypeEncoding(method)];
+    }
+    lastObjectInForwardingChain = forwardedObject;
+  } while ((forwardedObject = [forwardedObject forwardingTargetForSelector:sel]));
+  return [lastObjectInForwardingChain methodSignatureForSelector:sel];
 }
